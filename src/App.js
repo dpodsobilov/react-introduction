601df8a0d3278794528d47338035411847b9c7b8
Денис Подсобилов <dpodsobilov29@gmail.com>
2023-07-17 12:23:41 +0400

    creating posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+import React, { useState } from "react";
+import "./styles/App.css";
+import { PostItem } from "./components/PostItem";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
 
   return (
     <div className="App">
-      <ClassCounter />
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/App.css'
+
+export const PostItem = (props) => {
+    return (
+        <div className='post'>
+            <div className='post__content'>
+            <strong>{ props.post.id }. { props.post.title }</strong>
+            <div>
+                { props.post.body }
+            </div>
+            </div>
+            <div className='post_btns'>
+            <button>Удалить</button>
+            </div>
+        </div>
+    )
+}
