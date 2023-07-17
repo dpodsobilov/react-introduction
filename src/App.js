9f03ce119bbd4426c000065befe321d56d4a55d6
Денис Подсобилов <dpodsobilov29@gmail.com>
2023-07-17 13:11:09 +0400

    adding prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import "./styles/App.css";
-import { PostItem } from "./components/PostItem";
+import React, { useState } from 'react';
+import './styles/App.css';
+import { PostItem } from './components/PostItem';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "JavaScript", body: "Description" },
-    { id: 2, title: "JavaScript 2", body: "Description" },
-    { id: 3, title: "JavaScript 3", body: "Description" },
-  ]);
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'JavaScript', body: 'Description' },
+        { id: 2, title: 'JavaScript 2', body: 'Description' },
+        { id: 3, title: 'JavaScript 3', body: 'Description' },
+    ]);
 
-  return (
-    <div className="App">
-      {posts.map((post) => (
-        <PostItem post={post} key={post.id} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="App">
+            {posts.map((post) => (
+                <PostItem post={post} key={post.id} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
